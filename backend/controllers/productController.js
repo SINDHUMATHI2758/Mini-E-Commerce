4e4ea51775712c89a62160357b35b46d3dc870fd
SINDHMATHI <sindhusuji2005@gmail.com>
2025-05-12 15:35:48 +0530

    Initial commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,35 @@
+const productModel = require("../models/productModel");
+const ProductModel = require("../models/productModel");
+
+//Get Products API - /api/v1/products
+exports.getProducts = async (req, res, next) => {
+  let query = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+  const products = await ProductModel.find(query);
+  res.json({
+    success: true,
+    products,
+  });
+};
+
+//Get SingleProducts API - /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+    res.json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Unable to get Product with that ID",
+    });
+  }
+};
